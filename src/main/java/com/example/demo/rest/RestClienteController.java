b17cd8709fe12082be599ca37438a6d3d3f47515
Dipper <le.salcedo.99@gmail.com>
2021-11-16 13:53:04 -0500

    2021-11-16

--- src/main/java/com/example/demo/rest/RestClienteController.java
+++ src/main/java/com/example/demo/rest/RestClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Cliente;
@@ -19,6 +21,7 @@ import com.example.demo.service.IClienteService;
 
 @RestController
 @RequestMapping("/wscliente")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 public class RestClienteController {
 
 	@Autowired
@@ -35,7 +38,7 @@ public class RestClienteController {
 	}
 
 	@PostMapping
-	public ResponseEntity<Cliente> insertarCliente(@RequestBody Cliente c) {
+	public ResponseEntity<Cliente> insertarCliente(Cliente c) {
 		Cliente obj = clienteService.save(c);
 		if (obj != null) {
 			return new ResponseEntity<Cliente>(obj, HttpStatus.OK);
